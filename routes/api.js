564b61791ace4f5433a7d89fcb316acc6ca9f620
fullstack360 <info@fullstack360.com>
2016-06-28 19:06:47 -0400

    initial

---
+++ routes/api.js
@@ -0,0 +1,71 @@
+var express = require('express')
+var router = express.Router()
+var entryController = require('../controllers/EntryController')
+var controllers = {
+	entry: entryController
+}
+
+router.get('/:resource', function(req, res, next) {
+	var resource = req.params.resource
+
+	var controller = controllers[resource]
+	if (controller == null){
+	    res.json({
+	    	confirmation:'fail',
+	    	message: 'Invalid Resource'
+	    })
+
+		return
+	}
+
+	controller.get(req.query, false, function(err, results){
+		if (err){
+		    res.json({
+		    	confirmation:'fail',
+		    	message: err
+		    })
+			return
+		}
+
+	    res.json({
+	    	confirmation:'success',
+	    	results: results
+	    })
+
+		return
+	})
+})
+
+
+router.post('/:resource', function(req, res, next) {
+	var resource = req.params.resource
+
+	var controller = controllers[resource]
+	if (controller == null){
+	    res.json({
+	    	confirmation:'fail',
+	    	message: 'Invalid Resource'
+	    })
+
+		return
+	}
+
+	controller.post(req.body, function(err, result){
+		if (err){
+		    res.json({
+		    	confirmation:'fail',
+		    	message: err
+		    })
+			return
+		}
+
+	    res.json({
+	    	confirmation:'success',
+	    	result: result
+	    })
+
+		return
+	})
+})
+
+module.exports = router
